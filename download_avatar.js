9e75c1c83b22cd49b12a7f9422a7ba14de00ec31
Won Seob Shin <wonseob07@gmail.com>
2019-06-04 00:59:24 +0000

    added secrets and added to gitignore list for token import in main file

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,24 @@
 var request = require('request');
+var token = require('secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers : {
+      'User-Agent' : 'request'
+
+    }
+  };
+
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
