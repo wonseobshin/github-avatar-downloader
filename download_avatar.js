279eb6f475b2d5d40a57ebc667c47d9db0b30642
Won Seob Shin <wonseob07@gmail.com>
2019-06-04 02:44:20 +0000

    list avatar url on console and start on function to download jpg

--- download_avatar.js
+++ download_avatar.js
@@ -1,5 +1,6 @@
 var request = require('request');
-var token = require('secrets');
+var token = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,18 +8,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers : {
-      'User-Agent' : 'request'
-
+      'User-Agent' : 'request',
+      'Authorization' : token.GITHUB_TOKEN,
     }
   };
 
 
-  request(url, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  JSON.parse(result).forEach(function(element){
+    console.log(element.avatar_url);
+  })
 });
+
+function downloadImageByURL(url, filePath) {
+  fs.createWriteStream(filePath+'/'+url);
+}
